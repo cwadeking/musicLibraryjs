261567113744ad42d9e56f6cec16f286388fa6ea
cwadeking <charles.w.king.cwk@gmail.com>
2020-09-09 21:39:21 -0500

    function created for ajax call.  working on sourcing Jquery library correctly so that I can test

--- movieLibrary.js
+++ movieLibrary.js
@@ -1,2 +1,16 @@
 "use strict"
+function pageLoad(){
+    $.ajax({
+        url: `http://www.devcodecampmusiclibrary.com/api/music`, 
+        dataType: "json",
+        type: "get",
+        success: function(data, textStatus, jQxhr){
+    
+        },
+        error: function(jqXhr, textStatus, errorThrown){
+            console.log(errorThrown);
+        } 
+    });
+}
 
+pageLoad();
