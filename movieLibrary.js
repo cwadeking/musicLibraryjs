e8194cb00998cf9c087bdd07aa7cf36093b0214a
cwadeking <charles.w.king.cwk@gmail.com>
2020-09-10 18:30:26 -0500

    focussed on making an api call and displaying one piece of information from the json object.  Now that I know I can do that, I want to iterate over each object in the array and display the elements

--- movieLibrary.js
+++ movieLibrary.js
@@ -5,7 +5,10 @@ function pageLoad(){
         dataType: "json",
         type: "get",
         success: function(data, textStatus, jQxhr){
-    
+            $("#tableRow").append(`<td>${data[0].title}</td>`),
+            $("#tableRow").append(`<td>${data[0].album}</td>`),
+            $("#tableRow").append(`<td>${data[0].artist}</td>`),
+            $("#tableRow").append(`<td>${data[0].genre}</td>`)                               
         },
         error: function(jqXhr, textStatus, errorThrown){
             console.log(errorThrown);
