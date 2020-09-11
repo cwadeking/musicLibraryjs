320a74493cce81812818a2ab07cd739473d72238
cwadeking <charles.w.king.cwk@gmail.com>
2020-09-10 21:55:08 -0500

    trying to create a search bar that starts filtering the data set by each letter that's typed in the search bar.  after clearing the table and trying to repopulate it, the program will not repopulate the table

--- movieLibrary.js
+++ movieLibrary.js
@@ -24,4 +24,46 @@ function pageLoad(){
     });
 }
 
+var newArray = [];
+$(document).click(function(){
+    $( "#searchBar" ).keypress(function() {
+        $.ajax({
+            url: `http://www.devcodecampmusiclibrary.com/api/music`, 
+            dataType: "json",
+            type: "get",
+            success: function(data, textStatus, jQxhr){
+                var searchBoxInput = $("#searchBar").val();
+                
+                
+                for(let element = 0; element < data.length; element++){
+                    if(data[element].title.includes(searchBoxInput)){ //|| data[element].album.includes(searchBoxInput) 
+                    // || data[element].artist.includes(searchBoxInput) || data[element].genre.includes(searchBoxInput) 
+                    // || data[element].releaseDate.includes(searchBoxInput)){
+                        newArray.push(data[element]);
+                    }
+                    if(newArray.length === 0){
+                        pageLoad();
+                    }
+                }
+                $("td").empty();
+
+                for(let i = 0; i < newArray.length; i++){
+                    $("table").append(`<tr>
+                                        <td>${newArray[i].title}</td>
+                                        <td>${newArray[i].album}</td>
+                                        <td>${newArray[i].artist}</td>
+                                        <td>${newArray[i].genre}</td>
+                                        <td>${newArray[i].releaseDate}</td>
+                                    </tr>`)
+                    
+                }
+        },
+            error: function(jqXhr, textStatus, errorThrown){
+                console.log(errorThrown);
+            } 
+        });
+      });
+
+});
+
 pageLoad();
